a625d263ff311be6c437910b6c1fc3a4902ebbb0
santi0995 <santiserrato0910@gmail.com>
2023-03-09 15:21:54 +0100

    [feature] add sqlite table

--- src/screens/place-detail/index.jsx
+++ src/screens/place-detail/index.jsx
@@ -7,6 +7,9 @@ import { useSelector } from "react-redux";
 const PlaceDetail = ({ navigation, route }) => {
   const { placeId } = route.params;
   const place = useSelector((state) => state.place.places.find((place) => place.id === placeId));
+
+  const parseCoords = JSON.parse(place?.coords);
+
   return (
     <ScrollView style={styles.container}>
       <Image source={{ uri: place.image }} style={styles.image} />
@@ -14,8 +17,8 @@ const PlaceDetail = ({ navigation, route }) => {
         <View style={styles.addressContainer}>
           <Text style={styles.address}>{place.address}</Text>
         </View>
-        <MapPreview style={styles.map} location={{ lat: place.coords.lat, lng: place.coords.lng }}>
-          <Text>Ubicación no disponible</Text>
+        <MapPreview style={styles.map} location={{ lat: parseCoords.lat, lng: parseCoords.lng }}>
+          <Text>Ubicacion no disponible</Text>
         </MapPreview>
       </View>
     </ScrollView>
